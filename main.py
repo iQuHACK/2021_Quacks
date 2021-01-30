84457597fbdce1c0b66d0bba8edf477f717d5ed0
tanyagelizondo <tanya_yaretzi@live.com.mx>
2021-01-30 16:30:24 -0600

    the cards move with the mouse, adding sprites

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import pygame as pg
+from pygame.locals import *
 
 #Screen Size
-width= 800
-height= 600
+width= 1200
+height= 750
 
 pg.display.init()   
 screen = pg.display.set_mode((width,height))  
-screen.fill((255,255,255))
+#screen.fill((255,255,255))
 isRunning = True  
+
+card1 = pg.image.load('sprites/sprite1.png')
+card1.convert()
+rect = card1.get_rect()
+moving = False
 
 while isRunning:  
     for event in pg.event.get():  
-        if event.type == pg.QUIT:  
-            isRunning = False  
+        if event.type == QUIT: 
+            isRunning = False 
+
+        elif event.type == MOUSEBUTTONDOWN:
+            if rect.collidepoint(event.pos):
+                moving = True
+
+        elif event.type == MOUSEBUTTONUP:
+            moving = False
+
+        elif event.type == MOUSEMOTION and moving:
+            rect.move_ip(event.rel)
+        screen.fill((150, 150, 150))
+        screen.blit(card1, rect) 
         pg.display.update()
+pg.quit()
